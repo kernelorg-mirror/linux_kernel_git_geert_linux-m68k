2bfe154d9440958e723300743042d295393abae3
Michael Schmitz <schmitz@biophys.uni-duesseldorf.de>
2009-04-19 03:20:17 +0200

    fat: Use correct logical sector size for Atari GEMDOS FAT
    Fix erroneous use of device blocksize instead of logical sector size in
    Atari GEMDOS FAT code (GEMDOS uses a 16 bit FAT and a constant 2 logical
    sectors per cluster, and modifies the logical sector size to cope with the
    limits imposed by the 16 bit FAT). The bug was probably introduced by me when
    porting this code forward from 2.4.
    With this fix, I can successfully mount GEMDOS partitons up to 256 MB in size
    as prepared by Petr Stehlik. 512 MB requires 8k sector size, sorry.
    
    Signed-off-by: Michael Schmitz <schmitz@debian.org>
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>

--- fs/fat/inode.c
+++ fs/fat/inode.c
@@ -1805,7 +1805,7 @@ int fat_fill_super(struct super_block *sb, void *data, int silent, int isvfat,
 		 * it's a real MSDOS partition with 12-bit fat.
 		 */
 		if (!is_fat32(sbi) && total_clusters+2 > sbi->
-			fat_length*SECTOR_SIZE*8/sbi->fat_bits)
+			fat_length*sb->s_blocksize*8/sbi->fat_bits)
 			sbi->fat_bits = 12;
 		/* if it's a floppy disk --> 12bit fat */
 		if (!is_fat32(sbi) && MAJOR(sb->s_dev) == FLOPPY_MAJOR)
