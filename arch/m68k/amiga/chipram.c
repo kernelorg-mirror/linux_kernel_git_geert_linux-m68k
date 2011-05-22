f27c39e89ca74b84246aecfdbf4c297ad608b6ca
Geert Uytterhoeven <geert@linux-m68k.org>
2011-05-22 11:09:02 +0200

    m68k/amiga: Chip RAM - Offset resource end by CHIP_PHYSADDR
    Technically, the end of Chip RAM should be offset by CHIP_PHYSADDR (which is
    zero).
    
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>

--- arch/m68k/amiga/chipram.c
+++ arch/m68k/amiga/chipram.c
@@ -33,7 +33,7 @@ void __init amiga_chip_init(void)
     if (!AMIGAHW_PRESENT(CHIP_RAM))
 	return;
 
-    chipram_res.end = amiga_chip_size-1;
+    chipram_res.end = CHIP_PHYSADDR + amiga_chip_size - 1;
     request_resource(&iomem_resource, &chipram_res);
 
     chipavail = amiga_chip_size;
