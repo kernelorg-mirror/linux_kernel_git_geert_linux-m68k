ff6dd0d5bdb9354cbebbfc31406d718925e4dda4
Geert Uytterhoeven <geert@linux-m68k.org>
2011-05-24 22:21:01 +0200

    uAmiga: nocache

--- arch/m68k/include/asm/cacheflush_mm.h
+++ arch/m68k/include/asm/cacheflush_mm.h
@@ -130,42 +130,33 @@ static inline void __flush_page_to_ram(void *vaddr)
 	}
 }
 
+extern void flush_icache_user_range(struct vm_area_struct *vma, struct page *page,
+				    unsigned long addr, int len);
+extern void flush_icache_range(unsigned long address, unsigned long endaddr);
+
 #else /* !CONFIG_MMU */
 
-static inline void flush_icache(void)
-{
-}
-
-static inline void cache_clear(unsigned long paddr, int len)
-{
-}
-
-static inline void cache_push(unsigned long paddr, int len)
-{
-}
-
-static inline void cache_push_v(unsigned long vaddr, int len)
-{
-}
-
-static inline void flush_cache_mm(struct mm_struct *mm)
-{
-}
-
+static inline void flush_icache(void) { }
+static inline void cache_clear(unsigned long paddr, int len) { }
+static inline void cache_push(unsigned long paddr, int len) { }
+static inline void cache_push_v(unsigned long vaddr, int len) { }
+static inline void flush_cache_mm(struct mm_struct *mm) { }
 static inline void flush_cache_range(struct vm_area_struct *vma,
 				     unsigned long start,
-				     unsigned long end)
-{
-}
-
+				     unsigned long end) { }
 static inline void flush_cache_page(struct vm_area_struct *vma,
 				    unsigned long vmaddr, unsigned long pfn)
-{
-}
+{ }
+static inline void __flush_page_to_ram(void *vaddr) { }
+static inline void flush_icache_user_range(struct vm_area_struct *vma,
+					   struct page *page,
+					   unsigned long addr, int len) { }
+static inline void flush_icache_range(unsigned long address,
+				      unsigned long endaddr) {}
 
-static inline void __flush_page_to_ram(void *vaddr)
-{
-}
+static inline void __flush_dcache_all(void) {}
+
+#define flush_dcache_range(start, len)		__flush_dcache_all()
 
 #endif /* !CONFIG_MMU */
 
@@ -174,10 +165,6 @@ static inline void __flush_page_to_ram(void *vaddr)
 #define flush_dcache_mmap_lock(mapping)		do { } while (0)
 #define flush_dcache_mmap_unlock(mapping)	do { } while (0)
 #define flush_icache_page(vma, page)	__flush_page_to_ram(page_address(page))
-
-extern void flush_icache_user_range(struct vm_area_struct *vma, struct page *page,
-				    unsigned long addr, int len);
-extern void flush_icache_range(unsigned long address, unsigned long endaddr);
 
 static inline void copy_to_user_page(struct vm_area_struct *vma,
 				     struct page *page, unsigned long vaddr,
