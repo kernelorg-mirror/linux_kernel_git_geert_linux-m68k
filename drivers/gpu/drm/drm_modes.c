a1eb3580021afc5a6a50c8f049d78c66ab8c7177
Geert Uytterhoeven <geert@linux-m68k.org>
2022-06-23 15:16:54 +0200

    drm/modes: parse_cmdline: Make mode->*specified handling more uniform
    The various mode->*specified flags are not handled in an uniform way:
    some flags are set by the corresponding drm_mode_parse_cmdline_*()
    function, some flags by the caller of the function, and some flags by
    both.
    
    Make this uniform by making this the responsibility of the various
    parsing helpers, i.e.
      - Move the setting of mode->specified from caller to callee,
      - Drop the duplicate setting of mode->bpp_specified and
        mode->refresh_specified from callers.
    
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Reviewed-by: Hans de Goede <hdegoede@redhat.com>
    Acked-by: Thomas Zimmermann <tzimmermann@suse.de>
    Acked-by: Maxime Ripard <maxime@cerno.tech>
    ---
    v3:
      - Add Acked-by,
    
    v2:
      - Add Reviewed-by, Acked-by,

--- drivers/gpu/drm/drm_modes.c
+++ drivers/gpu/drm/drm_modes.c
@@ -1605,6 +1605,7 @@ static int drm_mode_parse_cmdline_res_mode(const char *str, unsigned int length,
 	mode->yres = yres;
 	mode->cvt = cvt;
 	mode->rb = rb;
+	mode->specified = true;
 
 	return 0;
 }
@@ -1921,8 +1922,6 @@ bool drm_mode_parse_command_line_for_connector(const char *mode_option,
 						      mode);
 		if (ret)
 			return false;
-
-		mode->specified = true;
 	}
 
 	/* No mode? Check for freestanding extras and/or options */
@@ -1944,8 +1943,6 @@ bool drm_mode_parse_command_line_for_connector(const char *mode_option,
 		ret = drm_mode_parse_cmdline_bpp(bpp_ptr, &bpp_end_ptr, mode);
 		if (ret)
 			return false;
-
-		mode->bpp_specified = true;
 	}
 
 	if (refresh_ptr) {
@@ -1953,8 +1950,6 @@ bool drm_mode_parse_command_line_for_connector(const char *mode_option,
 						     &refresh_end_ptr, mode);
 		if (ret)
 			return false;
-
-		mode->refresh_specified = true;
 	}
 
 	/*
